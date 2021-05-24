4be4c76cecf94b88dcf91f9a5cd85dcccad95a88
Nicochung <34371695+Nicochung@users.noreply.github.com>
2021-05-24 11:43:07 +0800

    Create 1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> sorted_array (nums.begin(), nums.end());
+        sort(sorted_array.begin(),sorted_array.end());
+        int i{}, j = nums.size() - 1;
+        while (i < j) {
+            int temp = sorted_array[i] + sorted_array[j];
+            if (temp == target) {
+                break;
+            } else if (temp > target) {
+                --j;
+            } else {
+                ++i;
+            }
+        }
+        vector<int>::iterator index1;
+        index1 = find(nums.begin(), nums.end(), sorted_array[i]);
+        int i1 = index1 - nums.begin();
+        
+        int i2 = 0;
+        for (int k = nums.size()-1; k >= 0; --k) {
+            if (nums[k] == sorted_array[j]) {
+                i2 = k;
+                break;
+            }
+        }
+        return vector<int>{i1, i2};
+    }
+};
