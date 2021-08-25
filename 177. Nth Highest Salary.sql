70a90158ea4c6c0aaa5b491d65f5da0493a785d0
Nicochung <34371695+Nicochung@users.noreply.github.com>
2021-08-25 13:47:30 +0800

    Update 177. Nth Highest Salary.sql

--- 177. Nth Highest Salary.sql
+++ 177. Nth Highest Salary.sql
@@ -1,15 +1,27 @@
+-- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- BEGIN
+--   DECLARE `offset` INT DEFAULT 0;
+--   SET `offset` = N - 1;
+--   RETURN (
+--       # Write your MySQL query statement below.
+--       SELECT IFNULL ( 
+--           (SELECT DISTINCT `Salary`
+--            FROM `Employee`
+--            ORDER BY `Salary` DESC
+--            LIMIT 1 OFFSET `offset`), 
+--           NULL
+--       )
+--   );
+-- END
+
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  DECLARE `offset` INT DEFAULT 0;
-  SET `offset` = N - 1;
+SET N = N - 1;
   RETURN (
       # Write your MySQL query statement below.
-      SELECT IFNULL ( 
-          (SELECT DISTINCT `Salary`
-           FROM `Employee`
-           ORDER BY `Salary` DESC
-           LIMIT 1 OFFSET `offset`), 
-          NULL
-      )
+      SELECT DISTINCT Salary
+      FROM Employee
+      ORDER BY Salary DESC
+      LIMIT N, 1
   );
 END
