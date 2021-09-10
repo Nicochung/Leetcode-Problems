82ea8a147c8f1140cc27684c5c392b398459cc8a
Nicochung <34371695+Nicochung@users.noreply.github.com>
2021-09-11 00:14:03 +0800

    Update 198. House Robber.cpp

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -16,16 +16,28 @@ public:
 //         return cache[pos];
 //     }
     
+//     int rob(vector<int>& nums) {
+//         if (nums.size() == 1) return nums[0];
+//         if (nums.size() == 2) return max(nums[0],nums[1]);  
+//         int n = nums.size();
+        
+//         nums[2] += nums[0];
+//         for (int i = 3; i < n; ++i) {
+//             nums[i] += max(nums[i-2], nums[i-3]);
+//         }
+        
+//         return max(nums[n-1], nums[n-2]);
+//     }
     int rob(vector<int>& nums) {
+        
         if (nums.size() == 1) return nums[0];
         if (nums.size() == 2) return max(nums[0],nums[1]);  
-        int n = nums.size();
         
-        nums[2] += nums[0];
-        for (int i = 3; i < n; ++i) {
-            nums[i] += max(nums[i-2], nums[i-3]);
+        // nums[i] represent the maximum at this index
+        nums[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size() ; i++) {
+            nums[i] = max(nums[i-1], nums[i-2] + nums[i]);
         }
-        
-        return max(nums[n-1], nums[n-2]);
+        return nums.back();
     }
 };
