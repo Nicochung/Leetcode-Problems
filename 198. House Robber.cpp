9aac339c42bd7fe3aed2b531d06379b4306db4b0
Nicochung <34371695+Nicochung@users.noreply.github.com>
2021-09-10 21:21:24 +0800

    Create 198. House Robber.cpp

---
+++ 198. House Robber.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+//     int rob(vector<int>& nums) {
+//         if (nums.size() == 1) return nums[0];
+//         if (nums.size() == 2) return max(nums[0],nums[1]);
+//         vector<int> cache(nums.size(),-1);
+        
+//         return max(maxProfitFromPos(nums, 0,cache), maxProfitFromPos(nums,1,cache));
+//     }
+    
+//     int maxProfitFromPos (vector<int>& nums, int pos, vector<int>& cache) {
+//         if (pos >= nums.size()) return 0;
+//         if (cache[pos] != -1) return cache[pos];
+//         cache[pos] = nums[pos] + max(maxProfitFromPos(nums, pos+2,cache), maxProfitFromPos(nums,pos+3,cache));
+        
+//         return cache[pos];
+//     }
+    
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0],nums[1]);  
+        int n = nums.size();
+        
+        nums[2] += nums[0];
+        for (int i = 3; i < n; ++i) {
+            nums[i] += max(nums[i-2], nums[i-3]);
+        }
+        
+        return max(nums[n-1], nums[n-2]);
+    }
+};
