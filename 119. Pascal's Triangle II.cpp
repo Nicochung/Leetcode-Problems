0a688238febd1c73aefb91271029afe542a6c1d6
Nicochung <34371695+Nicochung@users.noreply.github.com>
2021-09-11 19:13:26 +0800

    Update 119. Pascal's Triangle II.cpp

--- 119. Pascal's Triangle II.cpp
+++ 119. Pascal's Triangle II.cpp
@@ -15,8 +15,8 @@ public:
     vector<int> getRow(int rowIndex) {
         vector<int> ans(rowIndex+1, 1);
         
-        for (int i = 0; i < rowIndex; ++i) {
-            for (int j = 1; j < rowIndex-i; ++j) {
+        for (int i = 1; i < rowIndex; ++i) {
+            for (int j = 1; j <= rowIndex-i; ++j) {
                 if (j / 2 < rowIndex-i) ans[j] = ans[j] + ans[j-1];
                 else ans[j] = ans[j] + ans[j+1]; 
             }
