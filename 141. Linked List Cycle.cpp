c4dd5166b0b1c8bf8dde32fa3c266494f0c6b4f2
Nicochung <34371695+Nicochung@users.noreply.github.com>
2021-09-14 19:30:27 +0800

    Update 141. Linked List Cycle.cpp

--- 141. Linked List Cycle.cpp
+++ 141. Linked List Cycle.cpp
@@ -6,19 +6,32 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if (!head) return false;
+//         ListNode* fast = head->next;
+//         while (fast) {
+//             if (fast == head)
+//                 return true;
+//             head = head->next;
+//             fast = fast->next;
+//             if (fast)
+//                 fast = fast->next;
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (!head) return false;
-        ListNode* fast = head->next;
-        while (fast) {
-            if (fast == head)
-                return true;
-            head = head->next;
-            fast = fast->next;
-            if (fast)
-                fast = fast->next;
-        }
+        ListNode *slow{head}, *fast{head};
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }         
         return false;
     }
 };
