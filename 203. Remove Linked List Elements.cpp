edeebefa47b8fd79c76b818deff82b9b72040e8d
Nicochung <34371695+Nicochung@users.noreply.github.com>
2021-09-14 19:54:41 +0800

    Update 203. Remove Linked List Elements.cpp

--- 203. Remove Linked List Elements.cpp
+++ 203. Remove Linked List Elements.cpp
@@ -8,26 +8,66 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         // Empty List
+//         if (!head) return nullptr;
+//         ListNode *p = head, *q = nullptr;
+//         while (p) {
+//             if (p->val == val && !q) {
+//                 ListNode *temp = p;
+//                 p = p->next;
+//                 head = p;
+//                 delete temp;
+//             } else if (p->val == val && q){
+//                 ListNode *temp = p;
+//                 p = p->next;
+//                 q->next = p;
+//                 delete temp;
+//             } else {
+//                 q = p;
+//                 p = p->next;
+//             }
+//         }
+//         return head;
+//     }
+// };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        // Empty List
+        ListNode *prev{nullptr}, *trav{nullptr};
         if (!head) return nullptr;
-        ListNode *p = head, *q = nullptr;
-        while (p) {
-            if (p->val == val && !q) {
-                ListNode *temp = p;
-                p = p->next;
-                head = p;
-                delete temp;
-            } else if (p->val == val && q){
-                ListNode *temp = p;
-                p = p->next;
-                q->next = p;
+        // handle target val as first element
+        while (!prev && head) {
+            if (head->val == val) {
+                head = head->next;
+            } else {
+                trav = head->next;
+                prev = head;
+            }
+        }
+        
+        while (trav) {
+            if (trav->val == val) {
+                ListNode *temp = trav;
+                trav = trav->next;
+                prev->next = trav;
                 delete temp;
             } else {
-                q = p;
-                p = p->next;
+                prev = trav;
+                trav = trav->next;
             }
         }
         return head;
