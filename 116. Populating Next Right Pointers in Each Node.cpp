9dfa6ba56827c93e22f87ec39706b91685c3592e
Nicochung <34371695+Nicochung@users.noreply.github.com>
2021-09-15 14:04:46 +0800

    Update 116. Populating Next Right Pointers in Each Node.cpp

--- 116. Populating Next Right Pointers in Each Node.cpp
+++ 116. Populating Next Right Pointers in Each Node.cpp
@@ -18,25 +18,38 @@ public:
 
 class Solution {
 public:
+    // Node* connect(Node* root) {
+    //     if (!root) return root;
+    //     queue<Node*> q1;
+    //     q1.emplace(root);
+    //     int cnt{1}, cnt2{1};
+    //     while (!q1.empty()) {
+    //         Node *temp = q1.front();
+    //         q1.pop();
+    //         if (!temp) continue;
+    //         q1.emplace(temp->left);
+    //         q1.emplace(temp->right);
+    //         if (cnt == cnt2) {
+    //             cnt = 1;
+    //             cnt2 *= 2;
+    //         } else {
+    //             ++cnt;
+    //             temp->next = q1.front();
+    //         }
+    //     }
+    //     return root;
+    // }
+    void connect(Node* root1, Node* root2) {
+        if (!root1 && !root2) return;
+        root1->next = root2;
+        connect(root1->left, root1->right);
+        connect(root1->right, root2->left);
+        connect(root2->left, root2->right);
+    }
+    
     Node* connect(Node* root) {
-        if (!root) return root;
-        queue<Node*> q1;
-        q1.emplace(root);
-        int cnt{1}, cnt2{1};
-        while (!q1.empty()) {
-            Node *temp = q1.front();
-            q1.pop();
-            if (!temp) continue;
-            q1.emplace(temp->left);
-            q1.emplace(temp->right);
-            if (cnt == cnt2) {
-                cnt = 1;
-                cnt2 *= 2;
-            } else {
-                ++cnt;
-                temp->next = q1.front();
-            }
-        }
+        if (root == nullptr) return nullptr;
+        connect(root->left, root->right);
         return root;
     }
 };
